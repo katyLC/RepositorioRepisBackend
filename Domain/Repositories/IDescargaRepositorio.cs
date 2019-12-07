ca512842f0bdd3a96e93e3785229a9df286d0228
Katherine <kather.li.01@gmail.com>
2019-12-07 10:38:52 -0500

    App context

--- Domain/Repositories/IDescargaRepositorio.cs
+++
@@ -1,8 +0,0 @@
-using System.Threading.Tasks;
-using RespositorioREPIS.Data.DbModel;
-
-namespace RespositorioREPIS.Domain.Repositories {
-    public interface IDescargaRepositorio {
-        Task RegistrarDescarga(Descargas descargas);
-    }
-}
